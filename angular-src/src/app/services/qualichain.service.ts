689b75f4e6d72309ab0f91afa652b6d032db62e1
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2020-08-27 01:23:42 +0100

    chore: update endpoint url
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- angular-src/src/app/services/qualichain.service.ts
+++ angular-src/src/app/services/qualichain.service.ts
@@ -15,7 +15,7 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class QualichainService {
-    url: string = 'qualichain/validateCertificate';
+    url: string = 'http://localhost:8080/qualichain/validateCertificate';
   constructor(private http:Http, private authService: AuthService) { }
 
   validateCertificate(formData) {
@@ -30,65 +30,4 @@ export class QualichainService {
     // @ts-ignore
       return this.http.post(this.url, formData, { observe: 'events',  reportProgress: true }).pipe(map(res => res.json()));
   }
-  uploadCertificate(data) {
-      console.log("at validate")
-      console.log(data)
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    //optional
-    this.authService.loadTokenUser(headers);
-    return this.http.post('qualichain/validateCertificate', {data},
-        {headers: headers}).pipe(map(res => res.json()));
-  }
-
-    /*
-
-        public upload(
-        files: Set<File>
-    ): { [key: string]: { progress: Observable<number> } } {
-        // this will be the our resulting map
-        const status: { [key: string]: { progress: Observable<number> } } = {};
-
-        files.forEach(file => {
-            // create a new multipart-form for every file
-            const formData: FormData = new FormData();
-            formData.append('file', file, file.name);
-
-            // create a http-post request and pass the form
-            // tell it to report the upload progress
-            const req = new HttpRequest('POST', this.url, formData, {
-                reportProgress: true
-            });
-
-            // create a new progress-subject for every file
-            const progress = new Subject<number>();
-
-            // send the http-request and subscribe for progress-updates
-
-            const startTime = new Date().getTime();
-            this.http.request(req).subscribe(event => {
-                if (event.type === HttpEventType.UploadProgress) {
-                    // calculate the progress percentage
-
-                    const percentDone = Math.round((100 * event.loaded) / event.total);
-                    // pass the percentage into the progress-stream
-                    progress.next(percentDone);
-                } else if (event instanceof HttpResponse) {
-                    // Close the progress-stream if we get an answer form the API
-                    // The upload is complete
-                    progress.complete();
-                }
-            });
-
-            // Save every progress-observable in a map of all observables
-            status[file.name] = {
-                progress: progress.asObservable()
-            };
-        });
-
-        // return the map of progress.observables
-        return status;
-    }
-}
-     */
 }
