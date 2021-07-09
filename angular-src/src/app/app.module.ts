3a4fc6d68ee8448064b232b3c2eb817c45d5999c
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2021-07-09 02:27:36 +0100

    fix: delete unused component
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { LoginComponent } from './general/login/login.component';
 import { ProfileComponent } from './general/profile/profile.component';
 import { LoginStudentComponent } from './general/login/login-student/login-student.component';
 import { LoginSealComponent } from './general/login/login-seal/login-seal.component';
-import { AuthComponent } from './general/auth/auth.component';
 import { ChangePwComponent } from './general/login/change-pw/change-pw.component';
 
 const appRoutes: Routes = [
@@ -79,7 +78,6 @@ const appRoutes: Routes = [
         ConsortiumAuthComponent,
         LoginStudentComponent,
         LoginSealComponent,
-        AuthComponent,
         ChangePwComponent
 
     ],
